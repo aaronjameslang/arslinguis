3cadcd91e25d3f916c0916660f0005cd349ee536
Aaron J. Lang <aaronjameslang@googlemail.com>
2015-02-25 16:02:29 +0000

    Refactored db module

--- src/db.js
+++ src/db.js
@@ -3,6 +3,46 @@ var mongodb = require('mongodb');
 
 var deferred = Q.defer();
 
+module.exports = function() {
+	return deferred.promise;
+};
+
+var db = {};
+
+db.mongoify = function(criteria) {
+	if (!criteria || !criteria.id) {
+		return;
+	}
+	criteria._id = criteria.id;
+	delete criteria.id;
+	//return criteria;
+};
+
+db.demongoify = function(document) {
+	if (document === null) {
+		return;
+	}
+	document.id = document._id;
+	delete document._id;
+	//return document;
+};
+
+db.findOne = function() {
+	var deferred = Q.defer();
+	this.mongoify(arguments[0]);
+	function callback(error, document) {
+		if (error) {
+			deferred.reject(error);
+			return;
+		}
+		this.demongoify(document);
+		deferred.resolve(document);
+	}
+	Array.prototype.push.call(arguments, callback.bind(this));
+	this.collection.findOne.apply(this.collection, arguments);
+	return deferred.promise;
+};
+
 var url = 'mongodb://localhost/arslinguis';
 mongodb.MongoClient.connect(url, function(error, database) {
 	if (error) {
@@ -14,8 +54,8 @@ mongodb.MongoClient.connect(url, function(error, database) {
 			deferred.reject(error);
 			return;
 		}
-		deferred.resolve(collection);
+		db.collection = collection;
+		deferred.resolve(db);
 	});
 });
 
-module.exports = exports = deferred.promise;
--- test/db.js
+++ test/db.js
@@ -2,25 +2,27 @@ var chai = require('chai');
 var expect = chai.expect;
 chai.use(require('chai-as-promised'));
 
-var db;
+var getDb;
 
 describe('Database', function() {
 	it('should initialise without errors', function() {
-		db = require('../src/db.js');
+		getDb = require('../src/db.js');
+		expect(getDb()).to.be.an('object');
+		expect(getDb().then).to.be.a('function');
 	});
 	it('should connect succesfully', function() {
-		return expect(db).to.eventually.exist;
+		return getDb().then(function(db) {
+			expect(db).to.be.an('object');
+			expect(db.collection).to.be.an('object');
+			expect(db.findOne).to.be.a('function');
+		});
 	});
-	it('should respond with a document', function(done) {
-		db.then(function(collection) {
-			expect(collection).to.exist;
-			expect(collection.findOne).to.be.a.function;
-			collection.findOne({}, function(error, document) {
-				expect(error).not.to.exist;
-				expect(document).to.be.an.object;
-				expect(document.type).to.be.a.string;
-				done();
-			});
+	it('should respond with a document', function() {
+		return getDb().then(function(db) {
+			return db.findOne({});
+		}).then(function(document) {
+			expect(document).to.be.an('object');
+			expect(document.type).to.be.a('string');
 		});
 	});
 });
