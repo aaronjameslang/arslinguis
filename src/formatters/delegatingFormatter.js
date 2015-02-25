3630c4708ad5ec3e7c95f112b05d14ccf7b0bb1b
Aaron J. Lang <aaronjameslang@googlemail.com>
2015-02-25 08:46:57 +0000

    Fixed typos and ommisions

--- src/formatters/delegatingFormatter.js
+++ src/formatters/delegatingFormatter.js
@@ -1,17 +1,17 @@
 var _ = require('underscore');
 
 exports.findFormatter = function(mimeType) {
-	return _.find(this.formatter, function(formatter) {
+	return _.find(this.formatters, function(formatter) {
 		return formatter.canFormat(mimeType);
 	});
 };
 
 exports.canFormat = function(mimeType) {
-	return findFormatter(mimeType);
+	return this.findFormatter(mimeType);
 };
 
 exports.format = function(mimeType, data) {
-	return this.findFormatter(mimeType).mimeType(mimeType, data);
+	return this.findFormatter(mimeType).format(mimeType, data);
 };
 
 exports.formatters = [require('./jsonFormatter.js')];
