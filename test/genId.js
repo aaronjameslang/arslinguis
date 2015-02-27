ecdee54132d9d11e8c8bf68f9dffa14b369ae1d2
Aaron J. Lang <aaronjameslang@googlemail.com>
2015-02-27 16:45:49 +0000

    Added genId module & tests

---
+++ test/genId.js
@@ -0,0 +1,29 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+var genId = require('arsl/src/genId.js');
+
+var ids = [];
+
+function testId(id) {
+	describe('id: ' + id, function() {
+		it('should be 22 chars long', function() {
+			expect(id).to.have.length(22);
+		});
+		it('should should be unique', function() {
+			var index = ids.indexOf(id);
+			expect(index).to.equal(-1);
+			ids.push(id);
+		});
+		it('should be url safe', function() {
+			expect(id).to.equal(encodeURIComponent(id));
+		});
+	});
+}
+
+describe('genId', function() {
+	for (var index = 0 ; index < 16 ; index += 1) {
+		var id = genId();
+		testId(id);
+	}
+});
