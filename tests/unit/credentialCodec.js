ebc6403a69d65a47c31e8a3bfc942e956979e0f6
Aaron J. Lang <aaronjameslang@googlemail.com>
2015-07-22 20:08:39 +0100

    Removed function from loop, found false +ve tests
    Fixed tests, refactored fixtures

--- tests/unit/credentialCodec.js
+++ tests/unit/credentialCodec.js
@@ -3,34 +3,35 @@ var expect = chai.expect;
 
 var credentialCodec = require('../../libs/credentialCodec.js');
 
-var fixtures = {
-	"username:password" : {
+var fixtures = [
+	{
+		b64s: ['dXNlcm5hbWU6cGFzc3dvcmQ=', // username:password
+			'L3VzZXJuYW1lOnBhc3N3b3Jk'], // /username:password
 		username: 'username',
 		password: 'password'
-	},
-	"/username:password" : {
-		username: 'username',
-		password: 'password'
-	},
-	"domain/username:password" : {
+	}, {
+		b64s: ['ZG9tYWluL3VzZXJuYW1lOnBhc3N3b3Jk'], // domain/username:password
 		domain: 'domain',
 		username: 'username',
 		password: 'password'
-	},
-	"fish&chips:rock!!!": {
+	}, {
+		b64s: ['ZmlzaCUyNmNoaXBzOnJvY2shISE=', // fish%26chips:rock!!!
+			'ZmlzaCZjaGlwczpyb2NrISEh'], // fish&chips:rock!!!
 		username: 'fish&chips',
 		password: 'rock!!!'
-	},
-	"d%3A%2F%3F/dave%3F!%2F%3B%3A%2F://::123": {
+	}, {
+		b64s: ['ZCUzQSUyRiUzRi9kYXZlJTNGISUyRiUzQiUzQSUyRjovLzo6MTIz'], // d%3A%2F%3F/dave%3F!%2F%3B%3A%2F://::123
 		domain: 'd:/?',
 		username: 'dave?!/;:/',
 		password: '//::123'
 	}
-};
+];
 
 function testDecode(b64, credential) {
 	var actualCredential = credentialCodec.decode(b64);
-	expect(actualCredential).to.have.properties(credential);
+	expect(actualCredential.domain||'').to.equal(credential.domain||'');
+	expect(actualCredential.username).to.equal(credential.username);
+	expect(actualCredential.password).to.equal(credential.password);
 }
 
 function testEncode(b64, credential) {
@@ -39,14 +40,11 @@ function testEncode(b64, credential) {
 }
 
 describe('credentialCodec', function() {
-	for (var plain in fixtures) {
-		var b64 = new Buffer(plain).toString('base64');
-		var credential = fixtures[plain];
-		it('should be able to encode ' + plain, function() {
-			var actualB64 = credentialCodec.encode(credential);
-			expect(actualB64).to.equal(b64);
+	fixtures.forEach(function(fixture) {
+		console.log(fixture);
+		it('should be able to encode ' + fixture.b64s[0], testEncode.bind(null, fixture.b64s[0], fixture));
+		fixture.b64s.forEach(function(b64) {
+			it('should be able to decode ' + b64, testDecode.bind(null, b64, fixture));
 		});
-		it('should be able to decode ' + plain, testDecode.bind(null, b64, credential));
-		//it('should be able to encode ' + plain, testEncode.bind(null, b64, credential));
-	}
+	});
 });
