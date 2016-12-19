7420699a1fc2758d81c759168ecd7cb539c7e28e
Aaron J. Lang <aaronjameslang@googlemail.com>
2016-12-19 00:06:20 +0000

    Fix test

--- test/func/api/getLanguageJson.js
+++ test/func/api/getLanguageJson.js
@@ -26,9 +26,16 @@ describe('GET /language JSON', function () {
     var response = {
       statusCode: 200,
       headers: {
-        accept: 'application/json'
+        'content-type': 'application/json'
       },
-      _body: ''
+      _body: JSON.stringify({
+        'iso639_1': '',
+        'names': ['Thhtmaa'],
+        'descriptions': ['TODO: describe Thhtmaa by matt.pearson'],
+        'type': 'language',
+        'userId': '053a5c6f-7d03-44e4-8ec8-20728b97cb5f',
+        'id': 'd02e2092-0fb5-4d26-98a0-bfe879d379f8'
+      })
     }
     dispatch(done, request, response)
   })
